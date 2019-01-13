8c97fbda09f07a695912c3bbff36973665e18216
mohamedaldahoul <ma811forever@gmail.com>
2019-01-13 22:43:38 +0100

    update dependencies

--- src/Button.stories.js
+++ src/Button.stories.js
@@ -2,13 +2,14 @@ import React from 'react'
 // import {withInfo} from '@storybook/addon-info';
 import {wInfo} from './utils'
 
-import { storiesOf} from '@storybook/react'
-import {Button} from './Button'
+import { storiesOf } from '@storybook/react'
+import { Button } from './Button';
 import { text, boolean } from '@storybook/addon-knobs/react';
 // import { color } from '@storybook/addon-knobs/dist/deprecated';
 
 storiesOf('Button', module)
-  .addWithJSX('with background 1', 
+  .addWithJSX(
+    'with background 1', 
   wInfo(`
       description of the component
   
@@ -16,8 +17,7 @@ storiesOf('Button', module)
       <Button>Clich here</Button>
       ~~~
       
-    `
-    )(()=>(<Button bg={color('bg', "blue", 'group1')}>Hello World</Button>)))
+    `)(()=>(<Button bg={color('bg', "blue", 'group1')}>Hello World</Button>)))
   .addWithJSX(
     'with background 2', 
     ()=>(<Button bg={boolean('disabled', false)}>
